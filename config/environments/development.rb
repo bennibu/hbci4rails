1d2d3de8831ef20809fad08453171279a14243a9
Benjamin Meichsner <benni@posteo.de>
2010-05-06 22:06:00 +0200

    Enabled Hirb for development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,3 +15,10 @@ config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
+
+begin
+  require 'hirb'
+  Hirb.enable
+rescue
+  puts "Install hirb to get better active record lists."
+end
